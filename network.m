fa9dacea6a734716919a85801b55fa3dedf7077c
yzhizai <dsf_ihep@yeah.net>
2014-12-01 16:42:21 +0800

    change a function name

--- network.m
+++ network.m
@@ -10,7 +10,7 @@ weight_conversion.m %here the connection-lengths matrix is defined
                     %as the inverse of the connection-weights matrix.
                     
 %the next step is to obtain the distance matrix.
-distance.wei.m
+distance_wei.m
 
 %then use the next function to get the charactoristic path length.
 charpath.m
